49afda42c60f96256211eb636885ac0531bdcd7d
Stephan Moser <stephan.moser@student.tugraz.at>
2013-04-25 11:39:05 +0200

    Initial Commit

---
+++ AddSubTrainer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AddSubTrainer
+//
+//  Created by Stephan Moser on 4/25/13.
+//  Copyright (c) 2013 Stephan Moser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
